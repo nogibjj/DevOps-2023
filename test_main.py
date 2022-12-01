2e327ade5f94170468262b93670154efaebb9e75
Noah Gift <noah.gift@gmail.com>
2022-12-01 13:34:25 +0000

    getting to 100% test coverage

--- test_main.py
+++ test_main.py
@@ -3,8 +3,23 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+from mylib.calculator import add, subtract, multiply, divide
 
 
 def test_add():
     assert add(1, 2) == 3
+
+
+# create a multiply test
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+
+# create a subtract test
+def test_subtract():
+    assert subtract(3, 2) == 1
+
+
+# create a divide test
+def test_divide():
+    assert divide(4, 2) == 2
